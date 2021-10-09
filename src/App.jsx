603cc7640f1a485c17fd1f3c269f1e46bb904427
nkr413 <60217990+nkr413@users.noreply.github.com>
2021-10-09 05:48:48 +0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import styles from "./style.module.css";
 
 function App() {
+  window.addEventListener("keydown", (event) => {
+    console.log(event);
+  });
+
+
   return (
-    <div>
-      
+    <div class={styles.App}><br /><br />
+      <div class={styles.main_block}>
+        <div>Press any button</div><br />
+
+        <div class={styles.main_data_block}>
+          <div>
+            <div>Key Code</div><div></div>
+          </div>
+          <div>
+            <div>Key</div><div></div>
+          </div>
+          <div>
+            <div>Code</div><div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
