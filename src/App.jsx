7e8bb18b5c0c50595ad2516872bb0ada517d0f60
nkr413 <60217990+nkr413@users.noreply.github.com>
2021-10-09 05:18:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { render } from "solid-js/web";
+
+import "./b-style.css";
+import App from "./App";
+
+render(App, document.getElementById("root"));
