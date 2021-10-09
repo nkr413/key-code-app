2a9922c55f8bac6e871d8252b54b9950c04030d2
nkr413 <60217990+nkr413@users.noreply.github.com>
2021-10-09 06:15:23 +0500

    Update Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import styles from "./style.module.css";
+import { createSignal } from "solid-js";
 
 function App() {
+  const [getKeyCode, setKeyCode] = createSignal(Number());
+  const [getKey, setKey] = createSignal(String());
+  const [getCode, setCode] = createSignal(String());
+
   window.addEventListener("keydown", (event) => {
-    console.log(event);
+    if (event.keyCode == 32) {
+      setKey((prev) => prev = String("Space"));
+      setCode((prev) => prev = String("Space"));
+    }
+    else if (event.keyCode == 38) {
+      setKey((prev) => prev = String("ArrowUp &uarr;"));
+      setCode((prev) => prev = String("ArrowUp &uarr;"));
+    }
+    else if (event.keyCode == 37) {
+      setKey((prev) => prev = String("ArrowLeft &larr;"));
+      setCode((prev) => prev = String("ArrowLeft &larr;"));
+    }
+    else if (event.keyCode == 40) {
+      setKey((prev) => prev = String("ArrowDown &darr;"));
+      setCode((prev) => prev = String("ArrowDown &darr;"));
+    }
+    else if (event.keyCode == 39) {
+      setKey((prev) => prev = String("ArrowRight &rarr;"));
+      setCode((prev) => prev = String("ArrowRight &rarr;"));
+    }
+    else {
+      setKey((prev) => prev = String(event.key));
+      setCode((prev) => prev = String(event.code));
+    }
+
+    setKeyCode((prev) => prev = Number(event.keyCode));
   });
 
 
@@ -13,13 +43,13 @@ function App() {
 
         <div class={styles.main_data_block}>
           <div>
-            <div>Key Code</div><div></div>
+            <div>Key Code</div><div>{getKeyCode()}</div>
           </div>
           <div>
-            <div>Key</div><div></div>
+            <div>Key</div><div>{getKey()}</div>
           </div>
           <div>
-            <div>Code</div><div></div>
+            <div>Code</div><div>{getCode()}</div>
           </div>
         </div>
       </div>
